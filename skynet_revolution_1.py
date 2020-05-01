eb01f0e10b3b3404281836d4d7507ae435883a0e
ian <iansedano@gmail.com>
2020-05-01 22:34:35 +0200

    Finished Skynet, not true breadth first, but works.

--- skynet_revolution_1.py
+++ skynet_revolution_1.py
@@ -1,9 +1,6 @@
 import sys
 import math
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-
 # n: the total number of nodes in the level, including the gateways
 # l: the number of links
 # e: the number of exit gateways
@@ -13,33 +10,46 @@ nodes = {}
 for i in range(l):
     # n1: N1 and N2 defines a link between these nodes
     n1, n2 = [int(j) for j in input().split()]
-    nodes[j] = {1:n1, 2:n2}
+    nodes[i] = {1:n1, 2:n2}
 
 gateways = []
 for i in range(e):
     ei = int(input())  # the index of a gateway node
-    gateways.append[]
+    gateways.append(ei)
 
+print(gateways, file=sys.stderr)
 print(nodes, file=sys.stderr)
 
+critical_nodes=[]
+for g in gateways:
+    print(f"g {g}", file=sys.stderr)
+    for n in nodes.keys():
+        if nodes[n][1] == int(g) or nodes[n][2] == int(g):
+            critical_nodes.append(n)
 
+print(critical_nodes, file=sys.stderr)
 
 # game loop
 while True:
     si = int(input())  # The index of the node on which the Skynet agent is positioned this turn
-
+    print(f"si {si}", file=sys.stderr)
+    
     node_to_cut = ''
 
-    for i in nodes.Keys:
-        for j in nodes[l].Keys:
-
-
+    for cn in critical_nodes:
+        if nodes[cn][1] == si or nodes[cn][2] == si:
+            node_to_cut = nodes[cn]
+            critical_nodes.remove(cn)
     
 
-
+    for cn in critical_nodes:
+        if node_to_cut == '':
+            node_to_cut = nodes[critical_nodes[0]]
+            critical_nodes.pop(0)
+    
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr)
 
 
     # Example: 0 1 are the indices of the nodes you wish to sever the link between
-    print(node_to_cut)
+    print(f"{node_to_cut[1]} {node_to_cut[2]}")
